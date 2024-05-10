1178acc07df47b0fa844e0fe8f823e216fe128e8
Myeongjeon Lee <userclassdev@outlook.com>
2024-05-10 10:46:23 -0500

    add dialog

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -1,24 +1,35 @@
 import { useState } from 'react';
-import { FaStar } from 'react-icons/fa';
+import { FaStar, FaTimes } from 'react-icons/fa';
 
 function StarRating() {
   const [rating, setRating] = useState(0);
   const stars = Array(5).fill(0);
-
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
   return (
-    <div className='star-rating'>
-      <div className="stars">
-        {stars.map((_, index) => (
-          <FaStar
-            key={index}
-            color={index < rating ? 'gold' : 'grey'}
-            onClick={() => setRating(index + 1)}
-          />
-        ))}
+    <>
+      <div className='star-rating'>
+        <div className="stars">
+          {stars.map((_, index) => (
+            <FaStar
+              key={index}
+              color={index < rating ? 'gold' : 'grey'}
+              onClick={() => setRating(index + 1)}
+            />
+          ))}
+        </div>
+        {/* Add an "s" at the end if there's more than 1 star (plural) */}
+        <p>{rating} star{rating !== 1 && 's'}</p>
       </div>
-      {/* Add an "s" at the end if there's more than 1 star (plural) */}
-      <p>{rating} star{rating !== 1 && 's'}</p>
-    </div>
+      <button className="open-dialog-btn" onClick={() => setIsDialogOpen(true)}>Open Dialog</button>
+      {isDialogOpen && (
+        <div className="dialog">
+          <button className="close-btn" onClick={() => setIsDialogOpen(false)}>
+            <FaTimes />
+          </button>
+          <p>Hi, there.</p>
+        </div>
+      )}
+    </>
   );
 }
 
