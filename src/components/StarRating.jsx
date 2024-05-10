d18451cbae435fad5bdae0968f30bbf39a94028b
Myeongjeon Lee <userclassdev@outlook.com>
2024-05-10 10:22:02 -0500

    update CSS styles for StarRating component

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -6,14 +6,16 @@ function StarRating() {
   const stars = Array(5).fill(0);
 
   return (
-    <div>
-      {stars.map((_, index) => (
-        <FaStar
-          key={index}
-          color={index < rating ? 'gold' : 'grey'}
-          onClick={() => setRating(index + 1)}
-        />
-      ))}
+    <div className='star-rating'>
+      <div className="stars">
+        {stars.map((_, index) => (
+          <FaStar
+            key={index}
+            color={index < rating ? 'gold' : 'grey'}
+            onClick={() => setRating(index + 1)}
+          />
+        ))}
+      </div>
       {/* Add an "s" at the end if there's more than 1 star (plural) */}
       <p>{rating} star{rating !== 1 && 's'}</p>
     </div>
